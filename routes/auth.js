762001ef7566ba51147d3c32ceb7c463ccd788f8
Rafael Martinez <rafaelalfon@hotmail.com>
2024-11-30 20:52:32 -0600

    Fin Seccion 10.

--- routes/auth.js
+++ routes/auth.js
@@ -2,30 +2,30 @@
     path: api/login
 
 */
-const { Router } = require( 'express' );
-const { check } = require( 'express-validator' );
+const { Router } = require('express');
+const { check } = require('express-validator');
 
-const { crearUsuario, login, renewToken } = require( '../controllers/auth' );
-const { validarCampos } = require( '../middlewares/validar-campos' );
-const { validarJWT } = require( '../middlewares/validar-jwt' );
+const { crearUsuario, login, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 
 
-router.post( '/new', [
-  check( 'nombre', 'El nombre es obligatorio' ).not().isEmpty(),
-  check( 'password', 'La contraseña es obligatoria' ).not().isEmpty(),
-  check( 'email', 'El correo es obligatorio' ).isEmail(),
-  validarCampos
+router.post('/new', [
+    check('nombre','El nombre es obligatorio').not().isEmpty(),
+    check('password','La contraseña es obligatoria').not().isEmpty(),
+    check('email','El correo es obligatorio').isEmail(),
+    validarCampos
 ], crearUsuario );
 
-router.post( '/', [
-  check( 'password', 'La contraseña es obligatoria' ).not().isEmpty(),
-  check( 'email', 'El correo es obligatorio' ).isEmail(),
+router.post('/', [
+    check('password','La contraseña es obligatoria').not().isEmpty(),
+    check('email','El correo es obligatorio').isEmail(),
 ], login );
 
 
-router.get( '/renew', validarJWT, renewToken );
+router.get('/renew', validarJWT, renewToken );
 
 module.exports = router;
